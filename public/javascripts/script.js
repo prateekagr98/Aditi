72f41f54e42eec262fc5f623383be55c74736988
Prateek Agarwal <prateekagr98@gmail.com>
2015-03-24 22:16:53 +0530

    Add homepage slider structure

---
+++ public/javascripts/script.js
@@ -0,0 +1,45 @@
+(function () {
+
+    $(document).ready(function () {
+        var slider = new Slider(4000);
+        slider.start();
+    });
+
+    function Slider(timeInterval) {
+        this.timeInterval = timeInterval;
+
+        $('.homepage-slider-item').hide();
+        $('.homepage-slider-item').first().show();
+    };
+
+    Slider.prototype.start = function () {
+        this.slide();
+    };
+
+    Slider.prototype.slide = function () {
+        var self = this,
+            $targetEle = $('.homepage-slider-item:visible');
+
+        setTimeout(function () {
+            $('.homepage-slider').prepend($('<div>', {
+                'class': 'homepage-slider-loader'
+            }));
+        }, 1000);
+
+        setTimeout(function () {
+            $('.homepage-slider-loader').remove();
+            if ($targetEle.is(':last-child')) {
+                $targetEle.hide();
+                $('.homepage-slider-item').first().show();
+            } else {
+                $targetEle.hide().next().show();
+            }
+            self.slide();
+        }, self.timeInterval);
+    }
+
+    Slider.prototype.stop = function () {
+
+    }
+
+})();
