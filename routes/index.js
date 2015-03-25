4cd3b9d0f92dcdd3b3b97b0c60d2ffd43911f748
Prateek Agarwal <prateekagr98@gmail.com>
2015-03-25 20:56:09 +0530

    Show all images in homepage-slider as slider images

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    _ = require('underscore'),
+    glob = require('glob');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index');
+
+    glob("public/images/homepage-slider/*", null, function (er, imagePaths) {
+        var paths = _.map(imagePaths, function (item) {
+            return item.replace('public', '');
+        });
+        res.render('index', {
+            sliderSet: paths
+        });
+    });
 });
 
 module.exports = router;
