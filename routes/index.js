37431fd7895d6d1e04057120dc97443b27821878
Prateek Agarwal <prateekagr98@gmail.com>
2015-03-26 22:32:06 +0530

    Custom Gifts and Media and Portraits Page

--- routes/index.js
+++ routes/index.js
@@ -29,16 +29,54 @@ router.get('/oilPaintings', function (req, res, next) {
     });
 });
 
-router.get('/marbleArt', function (req, res, next) {
+router.get('/portraits', function (req, res, next) {
 
-    glob("public/images/marble-art/*", null, function (er, imagePaths) {
+    glob("public/images/portraits/*", null, function (er, imagePaths) {
         var paths = _.map(imagePaths, function (item) {
             return item.replace('public', '');
         });
-        res.render('marble-art', {
-            page: 'marble-art',
+        res.render('portraits', {
+            page: 'portraits',
             imageSet: paths
         });
+    });
+});
+
+router.get('/customGifts', function (req, res, next) {
+
+    glob("public/images/custom-gifts/*", null, function (er, imagePaths) {
+        var paths = _.map(imagePaths, function (item) {
+            return item.replace('public', '');
+        });
+        res.render('custom-gifts', {
+            page: 'custom-gifts',
+            imageSet: paths
+        });
+    });
+});
+
+router.get('/media', function (req, res, next) {
+
+    glob("public/images/media/*", null, function (er, imagePaths) {
+        var paths = _.map(imagePaths, function (item) {
+            return item.replace('public', '');
+        });
+        res.render('media', {
+            page: 'media',
+            imageSet: paths
+        });
+    });
+});
+
+router.get('/about', function (req, res, next) {
+    res.render('about', {
+        page: 'about'
+    });
+});
+
+router.get('/contact', function (req, res, next) {
+    res.render('contact', {
+        page: 'contact'
     });
 });
 
