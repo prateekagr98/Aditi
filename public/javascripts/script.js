eebbd069971346b1779c609e8f725ac1a9868c88
Prateek Agarwal <prateekagr98@gmail.com>
2015-03-27 22:33:44 +0530

    Added lightbox for all tabs

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
 (function () {
 
     $(document).ready(function () {
+        $('[rel="lightbox"]').lightbox();
         var slider = new Slider(4000);
         slider.start();
         loadSound();
